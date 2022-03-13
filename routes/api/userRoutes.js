328ffb9cde23f820c18e3643083e062d9b58cc1d
Carson <carsoncolgateccu@gmail.com>
2022-03-12 17:39:12 -0700

    user

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
